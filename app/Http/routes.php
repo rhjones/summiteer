e2786729a3cb6381f217da412746d7c1a2ff8464
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2015-11-18 08:59:44 -0500

    update debug route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,39 +28,39 @@ Route::controller('/hike','HikeController');
 if(App::environment('local')) {
 	
 	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-
-	Route::get('/debug', function() {
-
-	    echo '<pre>';
-
-	    echo '<h1>Environment</h1>';
-	    echo App::environment().'</h1>';
-
-	    echo '<h1>Debugging?</h1>';
-	    if(config('app.debug')) echo "Yes"; else echo "No";
-
-	    echo '<h1>Database Config</h1>';
-	    /*
-	    The following line will output your MySQL credentials.
-	    Uncomment it only if you're having a hard time connecting to the database and you
-	    need to confirm your credentials.
-	    When you're done debugging, comment it back out so you don't accidentally leave it
-	    running on your live server, making your credentials public.
-	    */
-	    //print_r(config('database.connections.mysql'));
-
-	    echo '<h1>Test Database Connection</h1>';
-	    try {
-	        $results = DB::select('SHOW DATABASES;');
-	        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-	        echo "<br><br>Your Databases:<br><br>";
-	        print_r($results);
-	    }
-	    catch (Exception $e) {
-	        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-	    }
-
-	    echo '</pre>';
-
-	});
 };
+
+Route::get('/debug', function() {
+
+    echo '<pre>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(config('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    /*
+    The following line will output your MySQL credentials.
+    Uncomment it only if you're having a hard time connecting to the database and you
+    need to confirm your credentials.
+    When you're done debugging, comment it back out so you don't accidentally leave it
+    running on your live server, making your credentials public.
+    */
+    //print_r(config('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    }
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    }
+
+    echo '</pre>';
+
+});
