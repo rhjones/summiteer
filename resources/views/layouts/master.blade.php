13293b7a357cf28185408360dff86079df83c0e3
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2015-12-11 16:24:56 -0500

    add Bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     </title>
 
     <meta charset="utf-8">
+    <link href="/css/app.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="/css/summiteer.css" rel="stylesheet">
 
