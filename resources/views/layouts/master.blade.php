70adbbf5e67c620b537f760da56ac162392190d9
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2015-12-15 16:26:17 -0500

    fix google fonts validation error

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:700|Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:700%7CPlayfair+Display:400,400italic' rel='stylesheet' type='text/css'>
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/summiteer.css" rel="stylesheet">
 
